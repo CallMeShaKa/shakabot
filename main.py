e9402561183189899f382f6df3a456cfab864dca
ShaKaOp <89745675+CallMeShaKa@users.noreply.github.com>
2022-07-13 18:08:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="shaka"),
+)
+
+
+bot.start()
+app.start()
+call_py.start()
+idle()
