8b5e2a4eb3460be5f7189d26c032908342a2db42
ShaKaOp <89745675+CallMeShaKa@users.noreply.github.com>
2022-07-13 18:06:05 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+admins = {}
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "VcVideoRobot")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "DeeCodeSupport")
+UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "DeeCodeBots")
+SOURCE_CODE = os.getenv("SOURCE_CODE", "github.com/Sammy-XD/VcVideoPlayer")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
