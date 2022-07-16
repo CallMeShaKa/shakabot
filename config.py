4039175275fb3ea7c73ea54cbfae6c20622bba78
ShaKaOp <89745675+CallMeShaKa@users.noreply.github.com>
2022-07-16 11:04:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_USERNAME = os.getenv("BOT_USERNSME")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL")
+SOURCE_CODE = os.getenv("SOURCE_CODE")
